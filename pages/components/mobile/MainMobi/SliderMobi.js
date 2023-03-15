e10a7f80987a04ec2bfb9baffd9758c8fa5098a2
Healer2602 <pray@taipei101.net>
2023-03-15 18:51:34 +0700

    update slider FE

--- pages/components/mobile/MainMobi/SliderMobi.js
+++ pages/components/mobile/MainMobi/SliderMobi.js
@@ -15,30 +15,58 @@ const slide_img = [
 ];
 
 const SliderMobi = () => {
-
-    return (
-        <div className="slide-mobi">
-          <Swiper
-            effect={"cards"}
-            grabCursor={true}
-            centeredSlides={true}
-            modules={[EffectCards]}
-            className="mySwiper"
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-          >
-            {slide_img.map((img, i) => {
-              return (
-                <SwiperSlide key={i}>
-                  <img src={img} alt="" />
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
-      );
+  return (
+    <div className="slide-mobi">
+      {/* <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        centeredSlides={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+      >
+        {slide_img.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={img} alt="" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper> */}
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 30,
+          stretch: 160,
+          depth: 575,
+          modifier: 1,
+          slideShadows: true,
+          scale: 1.25,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        className="mySwiper"
+      >
+        {slide_img.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={img} alt="" style={{ objectFit: "contain" }} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
 };
 
 export default SliderMobi;
